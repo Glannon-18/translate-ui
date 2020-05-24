7df4a494c69df3a600ddce420f1fcaa9ee699800
wkw <rced87@aliyun.com>
2020-05-24 22:15:46 +0800

    添加文本任务弹出框

--- src/components/Text_Task_Content.vue
+++ src/components/Text_Task_Content.vue
@@ -7,6 +7,7 @@
         <div style="margin-top: 10px">
             <el-button type="primary" icon="el-icon-download">下载</el-button>
             <el-button type="primary" icon="el-icon-delete">删除</el-button>
+            <el-button type="primary" icon="el-icon-plus" @click="showAdd">新建任务</el-button>
         </div>
         <div style="margin-top: 10px">
             <el-table
@@ -15,7 +16,7 @@
                     tooltip-effect="dark"
                     style="width: 100%"
                     @selection-change="handleSelectionChange"
-                    >
+            >
                 <el-table-column
                         type="selection"
                         width="55">
@@ -23,18 +24,18 @@
                 <el-table-column
                         prop="docName"
                         label="文档名称"
-                        width="120">
+                >
 
                 </el-table-column>
                 <el-table-column
                         prop="status"
                         label="状态"
-                        width="120">
+                >
                 </el-table-column>
                 <el-table-column
                         prop="original_language"
                         label="原文语种"
-                        >
+                >
                 </el-table-column>
                 <el-table-column
                         prop="hit"
@@ -43,7 +44,45 @@
                 </el-table-column>
             </el-table>
 
+            <el-pagination style="margin-top: 15px"
+                           layout="prev, pager, next"
+                           :total="100">
+            </el-pagination>
+
         </div>
+
+
+        <el-dialog title="新建文本任务" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+                <el-form-item label="任务名称名称" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="选择语种" :label-width="formLabelWidth">
+                    <el-select v-model="form.language" placeholder="请选择原文语种">
+                        <el-option label="中文" value="zh"></el-option>
+                        <el-option label="越南文" value="vi"></el-option>
+                        <el-option label="泰文" value="th"></el-option>
+                    </el-select>
+                </el-form-item>
+
+
+                <el-form-item label="文件上传" :label-width="formLabelWidth">
+                    <el-upload
+                            action=""
+                            :limit="3"
+                            :on-change="fileChange"
+                            :file-list="form.fileList" multiple
+                            :auto-upload="false">
+                        <el-button size="small" type="primary">点击上传</el-button>
+                        <div slot="tip">只能上传txt格式文件</div>
+                    </el-upload>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -53,8 +92,7 @@
         ,
         data() {
             return {
-                name: "这是一个文本翻译任务"
-                ,
+                name: "这是一个文本翻译任务",
                 tableData: [
 
                     {
@@ -107,14 +145,36 @@
                         hit: "未命中"
                     }
                 ],
-                multipleSelection: []
+                multipleSelection: [],
+                dialogFormVisible: false,
+
+                form: {
+                    name: "",
+                    language: "",
+
+                },
+                // 在 element ui 的 GitHub issue 中搜索了一下，发现 upload 组件确实不支持自动更新 fileList，
+                // 如果需要获取fileList，在回调函数里面获取。。。
+                fileList: [],
+                formLabelWidth: '120px'
             }
         }
         ,
-        methods:{
+        methods: {
             handleSelectionChange(val) {
                 this.multipleSelection = val;
                 console.log(this.multipleSelection)
+            },
+            showAdd() {
+                this.dialogFormVisible = true
+            },
+
+            fileChange(file, fileList) {
+                console.log('change')
+                console.log(file)
+                this.form.file = file.raw
+                console.log(this.form.file)
+                console.log(fileList)
             }
         }
     }
