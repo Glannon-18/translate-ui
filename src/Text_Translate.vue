6d71aff1c5be4f47392805fdd92933ad6f8f6356
myName <rced87@aliyun.com>
2020-05-28 12:02:34 +0800

    添加文本任务前台

--- src/Text_Translate.vue
+++ src/Text_Translate.vue
@@ -12,7 +12,9 @@
                     </div>
                 </div>
                 <TaskList :taskData.sync="task_data" :data_type="dataType">
-                    <el-button type="primary" v-if="btnshow" style="width: 100%" @click="dialogFormVisible=true">新建文本任务</el-button>
+                    <el-button type="primary" v-if="btnshow" style="width: 100%" @click="dialogFormVisible=true">
+                        新建文本任务
+                    </el-button>
                 </TaskList>
             </el-aside>
             <el-main>
@@ -21,11 +23,11 @@
 
         </el-container>
         <el-dialog title="新建文本任务" :visible.sync="dialogFormVisible" width="30%">
-            <el-form :model="form">
-                <el-form-item label="任务名称名称" :label-width="formLabelWidth">
+            <el-form :model="form" :rules="rules" ref="post_text_task">
+                <el-form-item label="任务名称名称" :label-width="formLabelWidth" prop="name">
                     <el-input v-model="form.name" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="选择语种" :label-width="formLabelWidth">
+                <el-form-item label="选择语种" :label-width="formLabelWidth" prop="language">
                     <el-select v-model="form.language" placeholder="请选择原文语种">
                         <el-option label="中文" value="zh"></el-option>
                         <el-option label="越南文" value="vi"></el-option>
@@ -37,9 +39,11 @@
                 <el-form-item label="文件上传" :label-width="formLabelWidth">
                     <el-upload accept=".txt"
                                name="multipartFile"
-                            action="/annexe_task/upload"
-                            :limit="9"
-                            multiple>
+                               action="/annexe_task/upload"
+                               :on-success="success"
+                               :on-remove="remove"
+                               :limit="9"
+                               multiple>
                         <el-button size="small" type="primary">点击上传</el-button>
                         <div slot="tip">只能上传txt格式文件</div>
                     </el-upload>
@@ -47,7 +51,7 @@
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary">确 定</el-button>
+                <el-button type="primary" @click="sub_form">确 定</el-button>
             </div>
         </el-dialog>
     </div>
@@ -69,7 +73,12 @@
                     language: "",
                 },
                 dialogFormVisible: false,
-                formLabelWidth: '120px'
+                formLabelWidth: '120px',
+                returnPaths: []
+                , rules: {
+                    name: [{required: true, message: '请输任务名称', trigger: 'blur'}],
+                    language: [{required: true, message: '请选择原文语言', trigger: 'blur'}],
+                }
             }
         }, methods: {
             getFast() {
@@ -85,6 +94,40 @@
                 this.getRequest("/annexe_task/listByDate", {name: ""}).then(resp => {
                     this.task_data = resp.data.obj
                     this.btnshow = true
+                })
+            },
+            success(response, file) {
+                this.returnPaths.push(file)
+                // console.log(this.returnPaths)
+            },
+            remove(file) {
+                let index = this.returnPaths.indexOf(file)
+                this.returnPaths.splice(index, 1)
+                // console.log(this.returnPaths)
+            }
+            ,
+            sub_form() {
+                this.$refs.post_text_task.validate(valid => {
+                    if (valid) {
+                        if (this.returnPaths.length == 0) {
+                            this.$message.error("请先上传文件")
+                            return
+                        }
+                        let fileList=[];
+                        this.returnPaths.forEach(r=>{
+                            fileList.push(r.response.obj)
+                        })
+
+
+                        this.postRequest("/annexe_task", {
+                            name: this.form.name,
+                            language: this.form.language,
+                            filePaths: fileList
+                        }).then(resp => {
+                            console.log(resp.data.obj)
+                        })
+                    }
+
                 })
             }
         },
